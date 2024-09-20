b80543b120b218556c1d99307cdcf4c244afec86
VishwasBJ <vishwasjai2004@gmail.com>
2024-09-20 22:00:26 +0530

    Create  End-of-file.java

---
+++ End-of-file.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       
+      int count=1;    
+        Scanner s = new Scanner(System.in);
+        
+        while(s.hasNext() == true) {
+            String ns = s.nextLine();
+            System.out.println(count + " " + ns);
+            count++;
+           
+    } 
+    }}
